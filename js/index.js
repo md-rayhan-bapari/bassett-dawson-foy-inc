24642889ecd412b35c0d7a97262b9be9ba03f056
rayhanwebDev4 <developerrayhan7@gmail.com>
2022-08-29 00:34:36 +0600

    added blog Section

--- js/index.js
+++ js/index.js
@@ -12,3 +12,18 @@ window.addEventListener('scroll', () => {
 	const header = document.querySelector('header');
 	header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+// Blog Slider
+const wrapper = document.querySelector('.resouces-wrapper');
+const indicators = [...document.querySelectorAll('.indicators button')];
+
+let currentTestimonial = 0; // Default 0
+
+indicators.forEach((item, i) => {
+	item.addEventListener('click', () => {
+		indicators[currentTestimonial].classList.remove('active');
+		wrapper.style.marginLeft = `-${100 * i}%`;
+		item.classList.add('active');
+		currentTestimonial = i;
+	});
+});
